7c36cb50601ce24bc119ca45cc168d766a42fd22
aya077 <AYA@gmail.com>
2022-12-08 09:11:15 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - entry 
- * return = 0
+ * Return: 0
  */
 
 int main(void)
 { 
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 
 
 
