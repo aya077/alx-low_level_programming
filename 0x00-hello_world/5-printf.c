a95f6ebb071645a5587a0e652a8a3af87a49bed4
aya077 <AYA@gmail.com>
2022-12-08 16:59:42 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - prints the phrase
+ * main - Entry Point
  *
- * Return 0
+ * Return Always 0
  */
 
 int main(void)
 {
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
