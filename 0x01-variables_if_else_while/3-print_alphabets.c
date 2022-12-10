f503c7956e6c22bd6612da2e614a05c3743bff3a
aya077 <AYA@gmail.com>
2022-12-10 10:21:25 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	
+
+	int i;
+	
+	
+	
+		
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar(toupper(alp[i]));
+	}
+	putchar('\n');
+	return (0);
+}
