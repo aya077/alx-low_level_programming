c5d34d6f63059ad85bfa788e19fbf6ddfa04e608
aya077 <AYA@gmail.com>
2022-12-11 15:54:03 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int c;
+
+	for (c = 0; c < 26; c--)
+	{
+		putchar(alp[c]);
+	}
+	putchar('\n');
+	return (0);
+}
