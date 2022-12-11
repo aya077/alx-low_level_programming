8704f3eaacf82293041d44624c25fe6081bd8494
aya077 <AYA@gmail.com>
2022-12-11 14:43:19 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * return: Always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
