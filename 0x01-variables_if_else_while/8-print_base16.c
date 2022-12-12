039fbd018b9252302cad0a70bd0372e94b9af609
aya077 <AYA@gmail.com>
2022-12-12 10:15:18 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+	int d;
+
+	c = 'a';
+	d = '0';
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+		}
+	putchar('\n');
+	return (0);
+}
