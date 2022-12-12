b4af0f34cb1ab8e53d4ec4495d4c98be18afe179
aya077 <AYA@gmail.com>
2022-12-12 12:06:46 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,9 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a < 100; a++)
-	{
+	for (a = 0; a < 100; a++) {
 		putchar(a + '0');
-		if (a < 99)
-		{
+		if (a < 99) {
 			putchar(',');
 			putchar(' ');
 		}
